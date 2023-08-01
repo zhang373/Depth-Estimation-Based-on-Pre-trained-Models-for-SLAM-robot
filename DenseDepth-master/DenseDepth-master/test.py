6b0fd148b2512736deeb2891bf205968c95e903a
zhang373 <1033187767@qq.com>
2023-08-01 20:43:55 +0800

    Updated files and code

--- DenseDepth-master/DenseDepth-master/test.py
+++ DenseDepth-master/DenseDepth-master/test.py
@@ -7,14 +7,14 @@ import matplotlib
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
-from tensorflow.keras.layers import Layer, InputSpec
+
 from utils import predict, load_images, display_images
 from matplotlib import pyplot as plt
 
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='examples/499_image.png', type=str, help='Input filename or folder.')
+parser.add_argument('--input', default='examples/*_image.png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
 # Custom object needed for inference and training
